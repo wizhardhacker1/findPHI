ce7226490e8aebceec748153bf87c7ca3f4cb645
DAS <46620390+wizhardhacker1@users.noreply.github.com>
2023-09-14 08:22:02 -0700

    Update findphi.py

--- findphi.py
+++ findphi.py
@@ -5,18 +5,32 @@ import openpyxl
 from PyPDF2 import PdfReader
 import warnings
 import threading
-import tkinter as tk
-from tkinter import filedialog, messagebox
-from tkinter.ttk import Progressbar
+import time
 
 warnings.filterwarnings("ignore")
+
+# Prompt the user for the path to search
+network_share_path = input("Enter the path to search: ")
+
+# Prompt the user for the path to save the report
+output_file_path = input("Enter the path to save the report (e.g., C:\\BGInfo\\CPRA.txt): ")
+
+# Define regular expressions for expected SSN and DOB formats
+ssn_pattern = r"\b\d{3}-\d{2}-\d{4}\b"  # Matches XXX-XX-XXXX
+dob_pattern = r"\b\d{2}/\d{2}/\d{4}\b"  # Matches XX/XX/XXXX
+
+# Define a simple pattern to search for the word "password"
+password_pattern = r"\bpassword\b"  # Matches the word "password"
+
+search_phrases = [ssn_pattern, dob_pattern, password_pattern, "birthdate"]
+file_extensions = ["docx", "xlsx", "pdf"]
 
 # Function to get password with timeout
 def get_password(timeout):
     password = None
     def input_thread():
         nonlocal password
-        password = entry_password.get()
+        password = input(f"Enter the decryption password within {timeout} seconds (or press Enter to skip): ")
 
     input_thread = threading.Thread(target=input_thread)
     input_thread.daemon = True
@@ -24,60 +38,57 @@ def get_password(timeout):
     input_thread.join(timeout)
     return password
 
-# Function to search and report
-def search_and_report():
-    network_share_path = entry_search_path.get()
-    output_file_path = entry_report_path.get()
+# Use a context manager to open the output file for writing
+with open(output_file_path, 'a') as f:
+    for root, dirs, files in os.walk(network_share_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_ext = file.split(".")[-1].lower()
+            if file_ext in file_extensions:
+                try:
+                    # Skip temporary Word files
+                    if file.startswith("~$"):
+                        continue
 
-    # Disable the Search button and set the message
-    button_search.config(state=tk.DISABLED)
-    label_status.config(text="Searching, please wait...")
+                    if file_ext == "docx":
+                        doc = docx.Document(file_path)
+                        file_contents = "\n".join([para.text for para in doc.paragraphs])
+                    elif file_ext == "xlsx":
+                        wb = openpyxl.load_workbook(file_path)
+                        sheet_names = wb.sheetnames
+                        sheet_contents = []
+                        for sheet_name in sheet_names:
+                            sheet = wb[sheet_name]
+                            for row in sheet.iter_rows():
+                                for cell in row:
+                                    if cell.value is not None:
+                                        sheet_contents.append(str(cell.value))
+                        file_contents = "\n".join(sheet_contents)
+                    elif file_ext == "pdf":
+                        pdf_file = open(file_path, 'rb')
+                        reader = PdfReader(pdf_file)
 
-    # Define regular expressions for expected SSN and DOB formats
-    ssn_pattern = r"\b\d{3}-\d{2}-\d{4}\b"  # Matches XXX-XX-XXXX
-    dob_pattern = r"\b\d{2}/\d{2}/\d{4}\b"  # Matches XX/XX/XXXX
+                        # Check if the PDF is encrypted
+                        if reader.is_encrypted:
+                            # Get the password with a timeout
+                            password = get_password(timeout=10)
 
-    # Define a simple pattern to search for the word "password"
-    password_pattern = r"\bpassword\b"  # Matches the word "password"
+                            if password:
+                                if reader.decrypt(password):
+                                    num_pages = len(reader.pages)
+                                    page_contents = []
+                                    for page_num in range(num_pages):
+                                        page = reader.pages[page_num]
+                                        page_text = page.extract_text()
+                                        page_contents.append(page_text)
+                                    file_contents = "\n".join(page_contents)
+                                else:
+                                    print(f"Failed to decrypt '{file_path}' with the provided password.")
+                                    continue  # Skip the encrypted PDF file
+                            else:
+                                print(f"Skipped '{file_path}' due to timeout.")
 
-    search_phrases = [ssn_pattern, dob_pattern, password_pattern, "birthdate"]
-    file_extensions = ["docx", "xlsx", "pdf"]
-
-    # Use a context manager to open the output file for writing
-    with open(output_file_path, 'a') as f:
-        for root, dirs, files in os.walk(network_share_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                file_ext = file.split(".")[-1].lower()
-                if file_ext in file_extensions:
-                    try:
-                        # Skip temporary Word files
-                        if file.startswith("~$"):
-                            continue
-
-                        if file_ext == "docx":
-                            doc = docx.Document(file_path)
-                            file_contents = "\n".join([para.text for para in doc.paragraphs])
-                        elif file_ext == "xlsx":
-                            wb = openpyxl.load_workbook(file_path)
-                            sheet_names = wb.sheetnames
-                            sheet_contents = []
-                            for sheet_name in sheet_names:
-                                sheet = wb[sheet_name]
-                                for row in sheet.iter_rows():
-                                    for cell in row:
-                                        if cell.value is not None:
-                                            sheet_contents.append(str(cell.value))
-                            file_contents = "\n".join(sheet_contents)
-                        elif file_ext == "pdf":
-                            pdf_file = open(file_path, 'rb')
-                            reader = PdfReader(pdf_file)
-
-                            # Check if the PDF is encrypted
-                            if reader.is_encrypted:
-                                # Skip encrypted PDFs automatically
-                                continue
-
+                        else:
                             num_pages = len(reader.pages)
                             page_contents = []
                             for page_num in range(num_pages):
@@ -86,71 +97,20 @@ def search_and_report():
                                 page_contents.append(page_text)
                             file_contents = "\n".join(page_contents)
 
-                        for search_phrase in search_phrases:
-                            if isinstance(search_phrase, str):
-                                # For non-regex search phrases, escape special characters
-                                pattern = re.escape(search_phrase)
-                            else:
-                                pattern = search_phrase
+                    for search_phrase in search_phrases:
+                        if isinstance(search_phrase, str):
+                            # For non-regex search phrases, escape special characters
+                            pattern = re.escape(search_phrase)
+                        else:
+                            pattern = search_phrase
 
-                            matches = re.findall(pattern, file_contents, re.IGNORECASE)
-                            if matches:
-                                # Write the findings to the output file
-                                f.write(f"Found '{search_phrase}' in file '{file_path}': {matches}\n")
+                        matches = re.findall(pattern, file_contents, re.IGNORECASE)
+                        if matches:
+                            # Write the findings to the output file
+                            f.write(f"Found '{search_phrase}' in file '{file_path}': {matches}\n")
 
-                    except Exception as e:
-                        # Print detailed error message
-                        print(f"Error reading '{file_path}': {e}")
+                except Exception as e:
+                    # Print detailed error message
+                    print(f"Error reading '{file_path}': {e}")
 
-                # Update the progress bar
-                progress_value.set((files.index(file) + 1) / len(files) * 100)
-                app.update_idletasks()
-
-    # Enable the Search button and set the completion message
-    button_search.config(state=tk.NORMAL)
-    label_status.config(text="Search completed.")
-    messagebox.showinfo("Search Completed", "Search and report generation completed.")
-
-# Create the main application window
-app = tk.Tk()
-app.title("File Search and Reporting")
-
-# Create and configure labels and entry fields
-label_search_path = tk.Label(app, text="Enter the path to search:")
-entry_search_path = tk.Entry(app)
-label_report_path = tk.Label(app, text="Enter the path to save the report:")
-entry_report_path = tk.Entry(app)
-label_password = tk.Label(app, text="Enter the decryption password (optional):")
-entry_password = tk.Entry(app, show="*")  # Password entry field
-
-# Create and configure buttons
-button_search = tk.Button(app, text="Search and Report", command=search_and_report)
-button_browse_search = tk.Button(app, text="Browse", command=lambda: entry_search_path.insert(0, filedialog.askdirectory()))
-button_browse_report = tk.Button(app, text="Browse", command=lambda: entry_report_path.insert(0, filedialog.asksaveasfilename(defaultextension=".txt")))
-
-# Create and configure a label for status messages
-label_status = tk.Label(app, text="")
-
-# Create a progress bar
-progress_value = tk.DoubleVar()
-progress_bar = Progressbar(app, mode="determinate", variable=progress_value)
-
-# Organize widgets using the grid layout
-label_search_path.grid(row=0, column=0, sticky="e")
-entry_search_path.grid(row=0, column=1, columnspan=2, sticky="ew")
-button_browse_search.grid(row=0, column=3)
-label_report_path.grid(row=1, column=0, sticky="e")
-entry_report_path.grid(row=1, column=1, columnspan=2, sticky="ew")
-button_browse_report.grid(row=1, column=3)
-label_password.grid(row=2, column=0, sticky="e")
-entry_password.grid(row=2, column=1, columnspan=2, sticky="ew")
-button_search.grid(row=3, column=0, columnspan=4)
-label_status.grid(row=4, column=0, columnspan=4)
-progress_bar.grid(row=5, column=0, columnspan=4, sticky="ew")
-
-# Configure column weights for resizing
-app.columnconfigure(1, weight=1)
-app.columnconfigure(2, weight=1)
-
-# Start the main event loop
-app.mainloop()
+print("Search and report generation completed.")

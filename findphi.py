140f356e1b534f9ef39b10cf595bda8e67f15caa
DAS <46620390+wizhardhacker1@users.noreply.github.com>
2023-09-20 12:12:31 -0700

    Update findphi.py

--- findphi.py
+++ findphi.py
@@ -1,26 +1,14 @@
+import html
 import os
-import docx
-import openpyxl
 import re
-from PyPDF2 import PdfReader
-import warnings
-import threading
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter.ttk import Progressbar
-import html
-
-warnings.filterwarnings("ignore")
-
-
-# Function to search for patterns within words using regular expressions
-def search_within_words(text, patterns):
-    found = []
-    for pattern, label in patterns:
-        for match in re.finditer(pattern, text, re.IGNORECASE):
-            found.append((label, match.group(0)))
-    return found
-
+import docx
+import openpyxl
+from PyPDF2 import PdfReader
+import datetime
+import dateutil.parser as parser
 
 # Function to add specific terms to the list
 def add_specific_terms():
@@ -28,36 +16,52 @@ def add_specific_terms():
     if terms_text:
         # Split the input text into individual terms
         terms = terms_text.split(',')
-
         for term in terms:
             term = term.strip()  # Remove leading/trailing spaces
             if term:
                 search_terms.append((html.escape(term), "Specific Term"))
         label_status.config(text="Specific Terms Added")
 
+# Function to search for patterns within words using regular expressions
+def search_within_words(text, patterns):
+    found = []
+    for pattern, label in patterns:
+        matches = re.finditer(pattern, text, re.IGNORECASE)
+        for match in matches:
+            found.append((label, match.group(0)))
+    return found
+
+# Function to parse a date-like string into a date object
+def parse_date(date_string):
+    try:
+        parsed_date = parser.parse(date_string, fuzzy=True)
+        return parsed_date.date()
+    except ValueError:
+        return None
 
 # Function to search and report
 def search_and_report():
     network_share_path = entry_search_path.get()
-    output_file_path = entry_report_path.get()
+    output_directory = entry_report_path.get()
 
     # Disable the Search button and set the message
     button_search.config(state=tk.DISABLED)
     label_status.config(text="Searching, please wait...")
 
+    # Define the search patterns
     search_patterns = [
         (r'\d{3}-\d{2}-\d{4}', "Possible SSN"),  # SSN-like pattern
-        (r'\d{2}/\d{2}/\d{4}', "Possible DOB"),  # DOB-like pattern (XX/XX/XXXX)
-        (r'\d{4}-\d{2}-\d{2}', "Possible DOB"),  # DOB-like pattern (XX-XX-XXXX)
-        (r'[A-Za-z\d@$!%*?&]{8,}', "Possible Password")  # Password-like pattern
+        (r'\d{2}/\d{2}/\d{4}', "Possible DOB"),  # DOB-like pattern (XX/XX/XXXX or XX-XX-XXXX)
     ]
 
     found_results = {
         "Possible SSN": [],
         "Possible DOB": [],
-        "Possible Password": [],
+        "Possible Password": [],  # Added a section for possible passwords
         "Specific Terms": []  # Added a section for specific terms
     }
+
+    today = datetime.date.today()
 
     for root, dirs, files in os.walk(network_share_path):
         for file in files:
@@ -108,13 +112,23 @@ def search_and_report():
 
                     # Append the findings to the appropriate column
                     for label, matches in found_patterns:
-                        if matches:
+                        if label == "Possible DOB":
+                            for dob in matches.split():
+                                dob_date = parse_date(dob)
+                                if dob_date is not None and dob_date <= today:
+                                    found_results[label].append((html.escape(file_path), html.escape(dob)))
+                        else:
                             found_results[label].append((html.escape(file_path), html.escape(matches)))
 
                     # Search for specific terms in file contents
                     for term, label in search_terms:
                         if term.lower() in file_contents.lower():
                             found_results["Specific Terms"].append((html.escape(file_path), term))
+
+                    # Check for potential passwords
+                    potential_passwords = find_potential_passwords(file_contents)
+                    for password in potential_passwords:
+                        found_results["Possible Password"].append((html.escape(file_path), html.escape(password)))
 
                 except Exception as e:
                     # Print detailed error message
@@ -129,35 +143,44 @@ def search_and_report():
     label_status.config(text="Search completed.")
     progress_value.set(0)  # Clear the progress bar
 
-    # Generate the HTML report with search terms
-    generate_html_report(output_file_path, found_results)
+    # Generate individual HTML reports for each section
+    generate_html_reports(output_directory, found_results)
 
     # Display a message box indicating the search is complete
-    messagebox.showinfo("Search Completed", "Search and report generation completed. Results saved to the output file.")
+    messagebox.showinfo("Search Completed", "Search and report generation completed. Results saved to the output directory.")
 
+# Function to generate individual HTML reports for each section
+def generate_html_reports(output_directory, found_results):
+    os.makedirs(output_directory, exist_ok=True)
 
-# Function to generate the HTML report
-def generate_html_report(output_file_path, found_results):
-    with open(output_file_path, 'w') as f:
-        f.write("<html><head><title>PHI Search Results</title></head><body>")
-        f.write("<h1>PHI Search Results</h1>")
+    for label, results in found_results.items():
+        if results:
+            file_name = f"{label.replace(' ', '_').lower()}_report.html"
+            file_path = os.path.join(output_directory, file_name)
 
-        # Display all results, including specific terms, horizontally
-        f.write("<div style='display: flex;'>")
-        for label, results in found_results.items():
-            if results:
-                f.write("<div style='margin-right: 20px;'>")
-                f.write(f"<h2>{label}</h2>")
+            with open(file_path, 'w') as f:
+                f.write("<html><head><title>PHI Search Results</title></head><body>")
+                f.write(f"<h1>{label}</h1>")
                 f.write("<table border='1' cellspacing='0' cellpadding='5'><tr><th>File Path</th><th>Matches</th></tr>")
                 for file_path, matches in results:
                     f.write(f"<tr><td>{file_path}</td><td>{matches}</td></tr>")
                 f.write("</table>")
-                f.write("</div>")
-        f.write("</div>")
+                f.write("</body></html>")
 
-        f.write("</body></html>")
+# Function to find potential passwords using a simple pattern (uppercase, lowercase, and digit)
+def find_potential_passwords(text):
+    potential_passwords = []
+    lines = text.split('\n')  # Split text into lines
 
+    pattern = r'(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@!#$%^&*#])[A-Za-z\d@!#$%^&*#]{5,}'  # Password pattern
 
+    for line in lines:
+        words = line.split()  # Split line into words
+        for word in words:
+            if re.match(pattern, word):  # Check if the word matches the pattern
+                potential_passwords.append(word)
+
+    return potential_passwords
 
 # Create the main application window
 app = tk.Tk()
@@ -166,17 +189,15 @@ app.title("File Search and Reporting")
 # Create and configure labels and entry fields
 label_search_path = tk.Label(app, text="Enter the path to search:")
 entry_search_path = tk.Entry(app)
-label_report_path = tk.Label(app, text="Enter the path/name to save the HTML report: ex report.html")
+label_report_path = tk.Label(app, text="Enter the output directory:")
 entry_report_path = tk.Entry(app)
 
 # Create and configure buttons
-button_search = tk.Button(app, text="Search and Report",
-                          command=lambda: threading.Thread(target=search_and_report).start())
+button_search = tk.Button(app, text="Search and Report", command=search_and_report)
 button_browse_search = tk.Button(app, text="Browse",
                                  command=lambda: entry_search_path.insert(0, filedialog.askdirectory()))
-button_browse_report = tk.Button(app, text="Browse", command=lambda: entry_report_path.insert(0,
-                                                                                              filedialog.asksaveasfilename(
-                                                                                                  defaultextension=".html")))
+button_browse_report = tk.Button(app, text="Browse",
+                                 command=lambda: entry_report_path.insert(0, filedialog.askdirectory()))
 
 # Create and configure a label for status messages
 label_status = tk.Label(app, text="")
